c7185c7798401d80baac5293ec085660b6a57579
AnnaAsritha <92287923+AnnaAsritha@users.noreply.github.com>
2024-07-15 11:53:58 +0000

    coversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33";
+// let score = null // null
+// let score = undefined // Nan
+// let score = true/fales // 1/0
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+let scope = "33abc"; // Nan
+
+
+
+let valueInNumbers = String(scope);
+
+//console.log(valueInNumbers);
+//console.log(typeof valueInNumbers);
+
+
+let bool = true;
+boolInconversion =  Boolean(bool);
+console.log(boolInconversion);
+
+let string = 12;
+
+conString = String(string);
+console.log(typeof conString);
+console.log(conString);
