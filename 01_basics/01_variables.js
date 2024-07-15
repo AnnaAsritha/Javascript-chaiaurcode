dca9bc32297d321fc1f8b7e2daecfe0e9846ae6e
AnnaAsritha <92287923+AnnaAsritha@users.noreply.github.com>
2024-07-15 09:07:32 +0000

    02_datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123;
+let accountEmail = 'annasritha@gamil.com';
+var accountPassword = '123456';
+accountCity = 'nandyal';
+let accountState;
+accountState = 'Karnataka'
+
+// accountId = 345; //Assignment to constant variable (constant variable cannot be changed again)
+
+accountEmail = 'hc.hc@gmail.com';
+accountPassword = '0998877';
+accountCity = 'bengaluru';
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountId,accountState]);
+
+
+/*
+    Prefer not to use var 
+    because of the block scope and functional scope
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as new server
+
+//alert(3 + 3); // gives error as we are using node not the javascript
+
+console.log('hut');
+
+let name = "asritha";
+let age = 12;
+let loggedIn = true;
+
+
+/* other data types are 
+primitive datatypes
+    number = 2 power of 53
+    string = ""
+    boolean = true/false
+    null = standlone value
+    undefined = the value is not defined
+    symbol -- for unique ness 
+*/
+
+//Object
+
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
+    
