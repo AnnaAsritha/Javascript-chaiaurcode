2ba64652379be3d4c8a59029eaf4608fcb0d670e
AnnaAsritha <92287923+AnnaAsritha@users.noreply.github.com>
2024-07-16 12:59:16 +0000

    datatype,comparision

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive 
+
+//7 types :string,Number,Boolean,null , undefined, bigInt
+
+const string = "asrith";
+const number =  90;
+const point = 90.3 //in js we dont have decimal
+const bool = true
+const nullies = null;
+let definite;
+
+const sym = symbol('123');
+const add = symbol('123');
+
+console.log(sym === add);
+
+const BigNumber = 39874879873287n;
+
+
+//Reference (Non Primitive)
+//Array , object , function
+
+Array = ['shal','ioii','lllll'];
+
+myObj = {
+    name: 'fdiuew',
+    age: 22
+}
+
+const myfunction = function(){
+    console.log('888');
+}
+
+console.log(typeof anotherId);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/*what is the difference between callbyvalue and callbyreference 
+it is just the difference how the memory is stored and how the memory is being accessd
+
+*/
