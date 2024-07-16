3f5d1f7eec55f2d4eb672280725203f4a9a83aa3
AnnaAsritha <92287923+AnnaAsritha@users.noreply.github.com>
2024-07-16 12:04:57 +0000

    converstion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,10 +22,48 @@ let valueInNumbers = String(scope);
 
 let bool = true;
 boolInconversion =  Boolean(bool);
-console.log(boolInconversion);
+// console.log(boolInconversion);
 
 let string = 12;
 
 conString = String(string);
-console.log(typeof conString);
-console.log(conString);
+// console.log(typeof conString);
+// console.log(conString);
+
+
+
+/*******************Operations *******************/
+
+/* 2+1 // add 
+   2-1 // sub
+   2*1 //mul
+   2/1 //divide
+   2%1 // remainder
+   2**5 // power
+*/
+
+let str1 = "hello";
+let str2 = "anna";
+
+let str3 = (str1 + str2);
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1+"2");//12
+// console.log("1"+"2");//12
+
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "3");//33
+
+
+// console.log((3 + 6) * 5 / 90);
+
+
+// console.log(+true); // 1
+// console.log(+"");
+// console.log(true+)//error
+
+
+let x = 2;
+ y = ++x;
+console.log(x);
